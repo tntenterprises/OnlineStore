26961a8a69c7aa4b77cfd42bfefb34ba0f0c4a5f
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-06 11:30:08 +0100

    ON-6 added basic UserController

---
+++ src/main/java/com/tnt/onlinestore/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.tnt.onlinestore.controllers;
+
+import com.tnt.onlinestore.entities.UserEntity;
+import com.tnt.onlinestore.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        UserEntity createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<UserEntity>> findUserById(@PathVariable Long id) {
+        Optional<UserEntity> foundUser = userService.findUserById(id);
+        return new ResponseEntity<>(foundUser, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<UserEntity>> findAllUsers() {
+        Iterable<UserEntity> allUsers = userService.findAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+}
