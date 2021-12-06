825be0d33c5c7c757917cfda7b0a8740204b62ea
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-06 11:18:32 +0100

    ON-6 added basic UserService class with create, getById, getAll and delete CRUD methods

---
+++ src/main/java/com/tnt/onlinestore/services/UserService.java
@@ -0,0 +1,39 @@
+package com.tnt.onlinestore.services;
+
+import com.tnt.onlinestore.entities.UserEntity;
+import com.tnt.onlinestore.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserEntity createUser(UserEntity userEntity) {
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+        return userRepository.save(userEntity);
+    }
+
+    public void deleteUser(Long id) {
+        UserEntity foundUser = userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        userRepository.deleteById(foundUser.getId());
+    }
+
+    public Optional<UserEntity> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<UserEntity> findAllUsers() {
+        return userRepository.findAll();
+    }
+}
