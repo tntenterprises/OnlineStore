fce75992a1e5a8fb7d97fbdc08b8fa0c8966e9e8
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-06 13:44:36 +0100

    ON-10 added cart test classes

---
+++ src/test/java/com/tnt/onlinestore/entities/CartEntityTest.java
@@ -0,0 +1,20 @@
+package com.tnt.onlinestore.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartEntityTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void removeProduct() {
+    }
+
+    @Test
+    void removeAllProducts() {
+    }
+}
