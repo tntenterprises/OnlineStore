d8e3840e235d21b0cb97c85a90bd38a5a48ce9a4
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-06 11:04:06 +0100

    ON-6 added UserRepository

---
+++ src/main/java/com/tnt/onlinestore/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tnt.onlinestore.repositories;
+
+import com.tnt.onlinestore.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByName (String name);
+}
