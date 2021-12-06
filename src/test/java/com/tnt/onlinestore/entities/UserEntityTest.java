d8a4601c5e15e8b6cf933e83fa27232af2db4e9a
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-06 11:41:40 +0100

    ON-6 added empty test classes for all classes

---
+++ src/test/java/com/tnt/onlinestore/entities/UserEntityTest.java
@@ -0,0 +1,16 @@
+package com.tnt.onlinestore.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserEntityTest {
+
+    @Test
+    void addRole() {
+    }
+
+    @Test
+    void removeRole() {
+    }
+}
---
+++ src/test/java/com/tnt/onlinestore/services/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.tnt.onlinestore.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void findAllUsers() {
+    }
+}
