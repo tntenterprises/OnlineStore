c4489c4bcf585693cba2fd17874ad739807a6917
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-12-10 15:43:15 +0100

    removed cartRepository from UserService

--- src/main/java/com/tnt/onlinestore/services/UserService.java
+++ src/main/java/com/tnt/onlinestore/services/UserService.java
@@ -3,7 +3,6 @@ package com.tnt.onlinestore.services;
 import com.tnt.onlinestore.entities.CartEntity;
 import com.tnt.onlinestore.entities.RoleEntity;
 import com.tnt.onlinestore.entities.UserEntity;
-import com.tnt.onlinestore.repositories.CartRepository;
 import com.tnt.onlinestore.repositories.RoleRepository;
 import com.tnt.onlinestore.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,17 +16,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final CartRepository cartRepository;
     private final CartService cartService;
     private final BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository, RoleRepository roleRepository,
-                       CartService cartService, BCryptPasswordEncoder bCryptPasswordEncoder, CartRepository cartRepository) {
+                       CartService cartService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.cartService = cartService;
         this.passwordEncoder = bCryptPasswordEncoder;
-        this.cartRepository = cartRepository;
     }
 
     public UserEntity createUser(UserEntity userEntity) {
